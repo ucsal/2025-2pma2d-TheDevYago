08e700d55bf83e91713806ea0203880adc41405a
TheDevYago <yagocpm2020@gmail.com>
2025-08-30 15:48:23 -0300

    fix: inverte dependencias do emailnotifier

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,16 @@ import br.com.mariojp.solid.dip.*;
 
 public class Main {
 	public static void main(String[] args) {
-		System.setProperty("DRY_RUN", "true"); // desejável não chamar SMTP
-		var notifier = new EmailNotifier();
-		// Estado inicial: vai lançar IllegalStateException (SMTP indisponível)
+		System.setProperty("DRY_RUN", "true");
+
+		MailSender sender;
+		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+			sender = new NoMailSender();
+		} else {
+			sender = new SmtpClient();
+		}
+
+		EmailNotifier notifier = new EmailNotifier(sender);
 		notifier.welcome(new User("Ana", "ana@example.com"));
 		System.out.println("Email enviado!");
 	}
